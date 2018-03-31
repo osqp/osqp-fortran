2b35df5378e8a1faf4d16006b61ddad59fc7b894
Bartolomeo Stellato <bartolomeo.stellato@gmail.com>
2018-03-31 12:13:55 -0400

    Try to add link overwrite for homebrew gcc osx

--- ci/travis/install.sh
+++ ci/travis/install.sh
@@ -21,7 +21,7 @@ if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
     sudo apt-get install -y gfortran
 else if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
     # gfortran is included in homebrew gcc
-    brew install --force gcc
+    brew install gcc; brew link --overwrite gcc
     fi
 fi
 
