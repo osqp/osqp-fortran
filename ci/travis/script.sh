9821c404e3287e354147eda3bd996d461440627c
Bartolomeo Stellato <bartolomeo.stellato@gmail.com>
2018-07-23 12:14:14 -0400

    Changes for QDLDL

--- ci/travis/script.sh
+++ ci/travis/script.sh
@@ -37,19 +37,5 @@ cmake -G "Unix Makefiles" -DCOVERAGE=ON ..
 make
 
 
-# Test OSQP 
+# Test OSQP
 ${TRAVIS_BUILD_DIR}/build/out/osqp_demo_fortran
-
-# # Pefrorm code coverage (only in Linux case for one version of python)
-# if [[ $TRAVIS_OS_NAME == "linux" ]]; then
-#     cd ${TRAVIS_BUILD_DIR}/build
-#     lcov --directory . --capture -o coverage.info # capture coverage info
-#     lcov --remove coverage.info "${TRAVIS_BUILD_DIR}/tests/*" \
-#         "${TRAVIS_BUILD_DIR}/lin_sys/direct/suitesparse/amd/*" \
-#         "${TRAVIS_BUILD_DIR}/lin_sys/direct/suitesparse/ldl/*" \
-#         "${TRAVIS_BUILD_DIR}/lin_sys/direct/suitesparse/SuiteSparse_config*" \
-#         "/usr/include/x86_64-linux-gnu/**/*" \
-#         -o coverage.info # filter out tests and unnecessary files
-#     lcov --list coverage.info # debug before upload
-#     coveralls-lcov coverage.info # uploads to coveralls
-# fi
